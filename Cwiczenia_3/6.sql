e4f1db18321e13b2fe1dd51aa709c77eca95cb3c
Agr3st <patryk.agr3st@gmail.com>
2024-01-19 23:06:24 +0100

    commit

--- Cwiczenia_3/6.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  DATE_PART('day', data) AS day,
-  DATE_PART('month', data) AS month
-FROM rozliczenia.godziny;
--- Cwiczenia_3/7.sql
+++
@@ -1,7 +0,0 @@
---SELECT * FROM rozliczenia.pensje;
-
-ALTER TABLE rozliczenia.pensje
-ADD COLUMN kwota_netto MONEY;
-
-UPDATE rozliczenia.pensje
-SET kwota_netto = kwota_brutto - (kwota_brutto * 0.17);
